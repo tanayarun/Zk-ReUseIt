71fc1bab8b835d71e24280192d577104c4a950f0
AnikethDeb <techrunstheworld@gmail.com>
2024-07-09 19:00:01 +0530

    feat:mapping-functions

--- Contracts/Ecommerce6.sol
+++ Contracts/Ecommerce6.sol
@@ -16,6 +16,9 @@ contract Ecommerce6 {
         uint256 time;
         Item item;
     }
+    mapping(uint256 => Item) public items;
+    mapping(address => mapping(uint256 => Order)) public orders;
+    mapping(address => uint256) public orderCount;
 
 
     event Buy(address buyer, uint256 orderId, uint256 itemId);
