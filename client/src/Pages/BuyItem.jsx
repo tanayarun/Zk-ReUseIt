a19a4375965f4508064cb1f80ec85593c123aa76
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-23 12:25:37 +0530

    feat-buy-shop

--- client/src/Pages/BuyItem.jsx
+++ client/src/Pages/BuyItem.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import MyContractABI from "../abis/abi.json";
 import Navbaar from "../Components/UI/Navbaar";
+import { Reclaim } from '@reclaimprotocol/js-sdk'
+import QRCode from "react-qr-code";
 
 const contractABI = MyContractABI;
 const contractAddress = "0x3e8bfE7377dd7df1b98DeF3e8Ae68Bcf41d6465D";
@@ -57,7 +59,69 @@ const BuyItem = () => {
 
     console.log("Bought");
   };
-
+  const [url, setUrl] = useState('')
+  
+  const APP_ID = "0x895eB26Be424Ee73603aBE2dd2A178a5D7a3787B" //TODO: replace with your applicationId
+  const reclaimClient = new Reclaim.ProofRequest(APP_ID)
+ 
+  async function generateVerificationRequest() {
+    try {
+      const providerId = '1d270ba2-8680-415b-b7e2-2cebd47f6f02'; // Replace with actual providerId
+  
+      reclaimClient.addContext(
+        ('user\'s address'), // Replace with actual user address
+        ('for acmecorp.com on 1st january')
+      );
+  
+      await reclaimClient.buildProofRequest(providerId);
+  
+      const signature = await reclaimClient.generateSignature(
+        '0x2c3b6e25c1fd61396ddd973d029dcb8a559a8ecb22fc32e89e9539ee4bca64a4'
+      );
+      reclaimClient.setSignature(signature);
+  
+      const { requestUrl, statusUrl } = await reclaimClient.createVerificationRequest();
+  
+      setUrl(requestUrl);
+  
+      await reclaimClient.startSession({
+        onSuccessCallback: proofs => {
+          console.log('Verification success', proofs);
+          // Your business logic here
+        },
+        onFailureCallback: error => {
+          console.error('Verification failed', error);
+          // Your business logic here to handle the error
+        }
+      });
+    } catch (error) {
+      console.error('Error in generateVerificationRequest:', error);
+    }
+  }
+  async function handleQRCode() {
+    try {
+      console.log('URL before fetch:', url); // Check if `url` is a string
+  
+      const response = await fetch('http://localhost:5001/callback/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ requestUrl: url }), // Ensure url is a string
+      });
+  
+      const result = await response.json();
+      if (result.success) {
+        console.log('Verification success', result.data);
+      } else {
+        console.error('Verification failed', result.error);
+      }
+    } catch (error) {
+      console.error('Error handling QR code request', error);
+    }
+  }
+  
+      
   return (
     <div>
       <Navbaar />
@@ -93,6 +157,17 @@ const BuyItem = () => {
         </button>
       </form>
 
+      <div  style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "50vh" }}>
+      {!url && (
+        <button className="text-white" onClick={generateVerificationRequest}>
+          Create Claim QrCode
+        </button>
+      )}
+      {url && (
+        <QRCode value={url} />
+      )}
+    </div>
+    <button onClick={handleQRCode} className="text-white">verify</button>
     </div>
   );
 };
--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import MyContractABI from "../abis/abi.json";
 import Navbaar from "../Components/UI/Navbaar";
+import ModalTSuccess from "../Components/UI/Modal";
+import eth from '../assets/eth.png'
+
+const contractABI = MyContractABI;
+const contractAddress = "0x654E671DBB480Dc3cC956Ee23C9A83163CeadE29";
 
 const Shop = () => {
   const [searchName, setSearchName] = useState("");
   const [fetchedItem, setFetchedItem] = useState(null);
   const [allItems, setAllItems] = useState([]);
-
-  const contractABI = MyContractABI;
-  const contractAddress = "0x654E671DBB480Dc3cC956Ee23C9A83163CeadE29";
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
 
   useEffect(() => {
     fetchAllItems();
@@ -69,6 +73,47 @@ const Shop = () => {
     }
   }
 
+  async function connectToMetamask() {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        return signer;
+      } catch (error) {
+        console.error("User denied account access");
+      }
+    } else {
+      console.error("Metamask is not installed");
+    }
+  }
+
+  async function buyItem(id, cost) {
+    const signer = await connectToMetamask();
+    if (!signer) return;
+
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    try {
+      const tx = await contract.buy(id, { value: ethers.utils.parseEther(cost) });
+      console.log("Transaction sent:", tx);
+
+      const receipt = await tx.wait();
+      console.log("Transaction mined:", receipt);
+
+      // Show success message in modal
+      setModalMessage("Transaction successful!");
+      setIsModalOpen(true);
+
+      // Listen for the Buy event
+      contract.on("Buy", (buyer, orderId, itemId) => {
+        console.log(`Item bought: Buyer = ${buyer}, Order ID = ${orderId}, Item ID = ${itemId}`);
+      });
+    } catch (error) {
+      console.error("Error buying item:", error.message);
+    }
+  }
+
   return (
     <div className="p-4">
       <Navbaar />
@@ -91,26 +136,42 @@ const Shop = () => {
 
       <div className="flex flex-wrap justify-center gap-4 pt-10">
         {fetchedItem && (
-          <div className=" text-white bg-blue-800 p-6 rounded-lg shadow-lg max-w-sm">
+          <div className="text-white bg-blue-800 p-6 rounded-lg shadow-lg max-w-sm">
+            <img src="https://via.placeholder.com/150" alt="Sample" className="w-full h-40 object-cover mb-4 rounded" />
             <p className="text-lg">ID: {fetchedItem.id}</p>
             <p className="text-lg">Name: {fetchedItem.name}</p>
             <p className="text-lg">Category: {fetchedItem.category}</p>
             <p className="text-lg">Cost: {fetchedItem.cost} ETH</p>
+            <button
+              onClick={() => buyItem(fetchedItem.id, fetchedItem.cost)}
+              className="mt-2 inline-block rounded bg-green-500 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-600 hover:shadow-xl focus:bg-green-600 focus:shadow-xl focus:outline-none active:bg-green-700"
+            >
+              Buy
+            </button>
           </div>
         )}
         {allItems.length > 0 && !fetchedItem && (
           <div className="flex flex-wrap justify-center gap-4">
             {allItems.map((item) => (
-              <div key={item.id} className=" text-white bg-blue-800 p-6 rounded-lg shadow-lg max-w-sm">
+              <div key={item.id} className="text-white bg-blue-800 p-6 rounded-lg shadow-lg max-w-sm">
+                <img src={eth} alt="Sample" className="w-20 object-cover mb-4 rounded" />
                 <p className="text-lg">ID: {item.id}</p>
                 <p className="text-lg">Name: {item.name}</p>
                 <p className="text-lg">Category: {item.category}</p>
                 <p className="text-lg">Cost: {item.cost} ETH</p>
+                <button
+                  onClick={() => buyItem(item.id, item.cost)}
+                  className="mt-2 inline-block rounded bg-green-500 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-600 hover:shadow-xl focus:bg-green-600 focus:shadow-xl focus:outline-none active:bg-green-700"
+                >
+                  Buy
+                </button>
               </div>
             ))}
           </div>
         )}
       </div>
+
+      <ModalTSuccess isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} message={modalMessage} />
     </div>
   );
 };
